144e4f25b5523f38eac208da02d56c3da04d2ab7
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-02-20 17:00:09 -0500

    adds react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import General from './General.js'
+
+function App() {
+  return (
+    <div>
+      <General />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/General.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function General() {
+    return (
+        <div>
+           <h1>General</h1>
+            <label>
+                Name:
+                <input type='text' className='name'></input>
+            </label>
+        </div>
+    )
+}
+
+export default General;
